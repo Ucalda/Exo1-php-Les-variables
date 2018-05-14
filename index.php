2bb5762457f750e442f4347cd1ca13dc0bb057e1
Ucalda <striker34120@gmail.com>
2018-05-14 19:10:13 +0200

    exo terminé

---
+++ index.php
@@ -0,0 +1,77 @@
+<html>
+<head>
+  <title>exo_php </title>
+</head>
+<body>
+<?php
+//Exercice 1
+$nom = "Didier";
+echo "$nom";
+echo "<br>";
+
+//exercise 2
+$nom = "Didier";
+echo "$nom";
+$prenom = "Super";
+echo "$prenom";
+$age = 99;
+echo "$age";
+echo "<br>";
+//exercise 3
+$km = 125;
+echo "$km";
+echo "<br>";
+
+//exercise 4
+$info = "Didier";
+echo $info;
+echo "\n";
+$age = 69;
+echo $age;
+echo "\n";
+$taille = 1.82;
+echo $taille;
+echo "\n";
+$uu = true;
+echo $uu;
+
+//exercise 5
+$agee =  NULL;
+echo $agee;
+echo "<br>";
+$agee = 42;
+echo $agee;
+echo "<br>";
+
+//exercise 6
+$nom = "Patosh";
+echo "Bonjour $nom comment va tu ?";
+echo "<br>";
+
+//exercise 7
+$nom = "Patosh";
+$prenom = "le maitre";
+$age = 99;
+echo "Bonjour $nom $prenom tu as $age ans";
+echo "<br>";
+
+//exercise 8
+$calcul1 = 3+4;
+$calcul2 = 5*20;
+$calcul3 = 45/5;
+echo $calcul1."\n".$calcul2."\n".$calcul3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
+</body>
